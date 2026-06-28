ea5306999747beadab93f1945fc29f4883193fef
Tushar Pavaiya <pavaiyatushar329@gmail.com>
2026-06-28 13:26:28 +0530

    update cors for vercel frontend

--- server.js
+++ server.js
@@ -9,8 +9,11 @@ const PORT=process.env.PORT||3000;
 
 connectDB();
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+  origin: [
+    "http://localhost:5173",
+    "https://authentication-frontend-fawn.vercel.app"
+  ],
+  credentials: true
 }));
 app.use(express.json());
 app.use('/user',userRoute);
